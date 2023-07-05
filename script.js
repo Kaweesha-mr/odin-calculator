cbfdaa4757b908b7a66243990fa1c1e66f5279d1
kaweesha-mr <kaweesha.mr@gmail.com>
2023-07-05 21:00:45 +0530

    things moved to old laptop

---
+++ script.js
@@ -0,0 +1,12 @@
+let display = document.getElementById("display-text");
+let buttons = document.getElementsByClassName("button");
+let operand1 = 0;
+let operand2 = null;
+let operator = null;
+
+for (let button of buttons) {
+
+    
+
+
+}
